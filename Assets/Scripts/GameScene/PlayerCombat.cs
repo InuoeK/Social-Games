3383dd802fc3d073bf76bcd4a5b67582abc7abfc
InuoeK <kevinprobably@live.ca>
2015-10-22 19:48:23 -0400

    22-10-2015_02
    Desktop controls added
    - WASD to move
    - mouse to shoot

--- Assets/Scripts/GameScene/PlayerCombat.cs
+++ Assets/Scripts/GameScene/PlayerCombat.cs
@@ -15,21 +15,37 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckJoyShootProjectiles();
+		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+			CheckMobileControls ();
+		
+		if(Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+			CheckComputerControls();
     }
 
-    private void CheckJoyShootProjectiles()
+    private void CheckMobileControls()
     {
         Vector2 aimJoy = cm.getAimingDirectionVec();
         if (cm.IsAimingJoyActive())
         {
-            GameObject t =
-            Instantiate(Resources.Load("proj"), GameObject.Find("Player").transform.position, Quaternion.identity) as GameObject;
+            GameObject t = Instantiate(Resources.Load("proj"), GameObject.Find("Player").transform.position, Quaternion.identity) as GameObject;
 
-            float atan = Mathf.Atan2(aimJoy.x, aimJoy.y);
             t.GetComponent<Rigidbody2D>().velocity = new Vector2(aimJoy.x * bulletSpeed, aimJoy.y * bulletSpeed);
-            Quaternion q = Quaternion.AngleAxis(atan * -180 / Mathf.PI, new Vector3(0, 0, 1));
+			Quaternion q = Quaternion.AngleAxis(Mathf.Atan2(aimJoy.x, aimJoy.y) * -180 / Mathf.PI, new Vector3(0, 0, 1));
             t.transform.rotation = q;
         }
     }
+
+	void CheckComputerControls()
+	{
+		if(Input.GetMouseButton(0))
+		{	
+			Vector2 normalizedVec = Camera.main.ScreenToWorldPoint(Input.mousePosition) - GameObject.Find ("Player").transform.position;
+			normalizedVec.Normalize();
+			GameObject t = Instantiate (Resources.Load ("proj"), GameObject.Find ("Player").transform.position, Quaternion.identity) as GameObject;
+				
+			t.GetComponent<Rigidbody2D>().velocity = new Vector2 (normalizedVec.x * bulletSpeed, normalizedVec.y * bulletSpeed);
+			Quaternion q = Quaternion.AngleAxis (Mathf.Atan2 (normalizedVec.x, normalizedVec.y) * -180 / Mathf.PI, new Vector3(0,0, 1));
+			t.transform.rotation = q;				
+		}
+	}
 }
--- Assets/Scripts/GameScene/PlayerMovement.cs
+++ Assets/Scripts/GameScene/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeedMult = 10.0f;
     private ControlModule cm;
     public float maxSpeed;
+
     private Rigidbody2D rb;
 
     // Use this for initialization
@@ -30,10 +31,39 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
-    private void MovePlayer()
+    void MovePlayer()
     {
-        Vector2 joyVec = cm.getMovementDirectionVec();
-        if (cm.IsMovementJoyActive() && joyVec != Vector2.zero && rb.velocity.x < maxSpeed){ rb.velocity = joyVec * moveSpeedMult;}
-        else { rb.velocity = Vector2.zero; }
+    	if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+			CheckMobileControls ();
+
+		if(Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+			CheckComputerControls();
     }
+
+	void CheckComputerControls()
+	{
+		Vector2 moveVec = new Vector2 (0, 0);
+		if (Input.GetKey ("w"))
+			moveVec.y += moveSpeedMult;
+
+		if (Input.GetKey ("a"))
+			moveVec.x -= moveSpeedMult;
+
+		if (Input.GetKey ("d"))
+			moveVec.x += moveSpeedMult;
+	
+		if (Input.GetKey ("s"))
+			moveVec.y -= moveSpeedMult;
+
+		 if (rb.velocity.magnitude < maxSpeed)
+			rb.velocity = moveVec;
+	
+	}
+
+	void CheckMobileControls()
+	{
+		Vector2 joyVec = cm.getMovementDirectionVec();
+		if (cm.IsMovementJoyActive() && joyVec != Vector2.zero && rb.velocity.x < maxSpeed){ rb.velocity = joyVec * moveSpeedMult;}
+		else { rb.velocity = Vector2.zero; }
+	}
 }
